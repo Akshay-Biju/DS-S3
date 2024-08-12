a93c9d49c554c4a3e81ffa773bab4267d181a045
Akshay-Biju <akshaytb2005@gmail.com>
2024-08-12 11:41:08 +0530

    Add files via upload

---
+++ Lab programs/bubblesort.c
@@ -0,0 +1,56 @@
+// Optimized implementation of Bubble sort
+#include <stdio.h>
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j,flag;
+    for (i = 0; i < n - 1; i++)
+    {
+        flag=0;
+        for (j = 0; j < n - i - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                flag=1;
+            }
+        }
+
+        // If no two elements were swapped by inner loop,
+        // then break
+        if (flag==0)
+            break;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+}
+
+// Driver program to test above functions
+int main()
+{
+    int i,n,arr[100];
+    printf("Enter size of array");
+    scanf("%d",&n);
+    printf("Enter elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
